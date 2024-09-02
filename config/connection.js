8ad012edf33d212d6de2a7244184ebe9f78c3f87
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-01 20:08:07 -0500

    Installed packaged and added route to the index.js file

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
+// replace with new created mongodb database for social network
 
 connect(connectionString);
 
