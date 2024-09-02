b2b57d14d6cfe4a22867ea0da5fb7396682fad95
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-01 21:17:21 -0500

    adding routes to connect to the server

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 const db = require('./config/connection');
+const routes = require('./routes/api');
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+// ?
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
 
 // app.listen((PORT) => {
 //     console.log("Listening on Port:", DB_PORT)
