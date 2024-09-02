038d1c4dcb89b3e3f42a087d563f40c27162fb2f
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-01 21:07:30 -0500

    setting up the documents and collections in mongoose, and adding a connection to the local index file

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
+const connectionString = 'mongodb://localhost:27017/socialNetworkDB';
 // replace with new created mongodb database for social network
 
 connect(connectionString);
 
 module.exports = connection;
+
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+// app.listen((PORT) => {
+//     console.log("Listening on Port:", DB_PORT)
+// })
+// app.listen(3000)
+
+// app.listen(3000, () => {
+//     console.log('Server listening on http://localhost:3000');
+//   })
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
