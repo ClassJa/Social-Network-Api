1bf8bed9867c6bd055150074f49233fd328ea679
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-02 20:43:22 -0500

    completed the thoughtsRoutes file, added the routes and functions that will be exported from this file

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,3 +1,22 @@
 const router = require('express').Router();
 
+const {
+    getThoughts,
+    getSingleThoughts,
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+  } = require('../../controllers/thoughtsController.js');
+  
+
+  router.route('/').get(getThoughts).post(createThoughts);
+
+
+  router
+  .route('/:courseId')
+  .get(getSingleThoughts)
+  .put(updateThoughts)
+  .delete(deleteThoughts);
+
+
 module.exports = router;
