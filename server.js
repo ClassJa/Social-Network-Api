edf207a2dc48e994fa3cc318da7a9d7e24b299c2
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-11 13:22:06 -0500

    debugging the server connection

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express')
 const app = express()
 const db = require('./config/connection');
-// const routes = require('./routes/api');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
-
-// app.get('/', function (req, res) {
-//   res.send()
-  // res.send('Hello World')
-// })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
